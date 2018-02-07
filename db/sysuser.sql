b41ca8e850f1586b1fcfcc9c4eca38c6d7b8aeb7
yangdl70_chinaunicom <yangdl70@chinaunicom.cn>
2018-02-07 20:47:38 +0800

    脚本

---
+++ db/sysuser.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog v10.2 
+MySQL - 5.6.31 : Database - 5ienglish
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`5ienglish` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `5ienglish`;
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `login_name` varchar(100) COLLATE utf8_bin NOT NULL COMMENT '登录名',
+  `password` varchar(100) COLLATE utf8_bin NOT NULL COMMENT '密码',
+  `no` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '工号',
+  `name` varchar(100) COLLATE utf8_bin NOT NULL COMMENT '姓名',
+  `email` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '电话',
+  `mobile` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '手机',
+  `user_type` char(1) COLLATE utf8_bin DEFAULT NULL COMMENT '用户类型 0学生 1老师 2管理人员',
+  `photo` varchar(1000) COLLATE utf8_bin DEFAULT NULL COMMENT '用户头像',
+  `login_ip` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '最后登陆IP',
+  `login_date` datetime DEFAULT NULL COMMENT '最后登陆时间',
+  `login_flag` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT '是否可登录',
+  `create_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT '更新者',
+  `update_date` datetime DEFAULT NULL COMMENT '更新时间',
+  `remarks` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) COLLATE utf8_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`),
+  KEY `sys_user_login_name` (`login_name`),
+  KEY `sys_user_del_flag` (`del_flag`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='用户表';
+
+/*Data for the table `sys_user` */
+
+insert  into `sys_user`(`id`,`login_name`,`password`,`no`,`name`,`email`,`phone`,`mobile`,`user_type`,`photo`,`login_ip`,`login_date`,`login_flag`,`create_by`,`create_date`,`update_by`,`update_date`,`remarks`,`del_flag`) values (1,'thinkgem','123456','0001','系统管理员','thinkgem@163.com','8675','8675','','','0:0:0:0:0:0:0:1','2017-10-15 18:56:48','1','1','2013-05-27 08:00:00','1','2017-09-11 22:04:40','最高管理员','0'),(2,'sd_admin','123456','0002','管理员',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(3,'sd_zhb','123456','0003','综合部',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(4,'sd_scb','123456','0004','市场部',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(5,'sd_jsb','123456','0005','技术部',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(6,'sd_yfb','123456','0006','研发部',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(7,'jn_admin','123456','0007','济南领导',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(8,'jn_zhb','123456','0008','济南综合部',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(9,'jn_scb','123456','0009','济南市场部',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(10,'jn_jsb','123456','0010','济南技术部',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(11,'lc_admin','123456','0011','济南历城领导',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(12,'lx_admin','123456','0012','济南历下领导',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(13,'gx_admin','123456','0013','济南高新领导',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'1','1','2013-05-27 08:00:00','1','2013-05-27 08:00:00',NULL,'0'),(14,'yangdl70','123456',NULL,'杨大龙',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'0');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
